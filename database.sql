c361a3dd4cd64387a40c0f5c4dfe9fcc38995159
dmheisel <d.m.heisel@gmail.com>
2019-08-09 16:38:17 -0500

    added database.sql with create and starter tasks

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "tasks"
+(
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(80) NOT NULL,
+  "details" VARCHAR(256),
+  "isDone" BOOLEAN DEFAULT FALSE
+);
+
+INSERT
+	INTO "tasks"
+  ("name", "details")
+VALUES
+  ('Garbage', 'Take out the garbage.'),
+  ('Mow', 'Mow the lawn and use the weedwhacker this time.'),
+  ('Homework', 'Finish the weekend assignment for Prime.'),
+  ('Bike Maintenance', 'Wash the bike, clean the chains, tune the shifting, change brakepads.'),
+  ('Oil Change - Jess', 'Change the oil in the car, check with Jess if this was done.');
