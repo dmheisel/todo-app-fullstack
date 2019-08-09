72b10854b0aa39a2936a5b219ae9c607eb137ebe
dmheisel <d.m.heisel@gmail.com>
2019-08-09 16:58:30 -0500

    server/router set up, POST and GET routes established

---
+++ server/routes/taskRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+
+const Pool = pg.Pool;
+const pool = new Pool({
+	database: 'DTD_To_Do_List',
+	host: 'localhost',
+	port: 5432,
+	max: 15,
+	idleTimeoutMillis: 20000
+});
+
+module.exports = router;
+
+//get all tasks from database
+router.get('/', (req, res) => {
+	let queryText = `SELECT * FROM "tasks";`;
+	pool
+		.query(queryText)
+		.then(result => {
+			console.log('successful GET request from database');
+			res.send(result.rows);
+		})
+		.catch(err => {
+			console.log('error on database GET request: ', err);
+			res.sendStatus(500);
+		});
+});
+
+//post new task to database
+router.post('/', (req, res) => {
+	let newTask = req.body;
+	let queryText = `INSERT INTO "tasks" ("name", "details")
+                    VALUES ($1, $2);`;
+	let values = [newTask.name, newTask.details];
+	pool
+		.query(queryText, values)
+		.then(result => {
+			console.log('successful POST request to database');
+			res.sendStatus(201);
+		})
+		.catch(err => {
+			console.log('error on database POST request: ', err);
+			res.sendStatus(500);
+		});
+});
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const taskRouter = require('./routes/taskRouter')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+app.use('/tasks', taskRouter);
+
+app.listen(PORT, () => console.log('app is listening on port: ', PORT));
