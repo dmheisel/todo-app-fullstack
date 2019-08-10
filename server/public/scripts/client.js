314b4cea4cacff99246b31687c01ca7365f507b2
dmheisel <d.m.heisel@gmail.com>
2019-08-10 11:57:27 -0500

    completed input fields feature

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,12 +3,44 @@ console.log('js sourced');
 $(document).ready(readyHandler);
 
 function readyHandler() {
-	$('#addButton').on('click', onAddClick )
+	$('#addButton').on('click', onAddClick);
 	getTasks();
 }
 
-const 
+function clearInputs() {
+	$('#taskName').val('');
+	$('#taskDetails').val('');
+}
 
+function onAddClick() {
+	if ($('#taskName').val() !== '') {
+		let taskObj = {
+			name: $('#taskName').val(),
+			details: $('#taskDetails').val()
+		};
+		postTask(taskObj);
+	} else {
+		alert('Task must have name field completed before submitting.');
+		return;
+	}
+}
+
+function postTask(task) {
+	$.ajax({
+		method: 'POST',
+		url: '/tasks',
+		data: task
+	})
+		.then(response => {
+			console.log('successful POST route to server: ', response);
+			clearInputs();
+			$('#inputFields').collapse('hide');
+			getTasks();
+		})
+		.catch(err => {
+			console.log('error on POST route to server: ', err);
+		});
+}
 
 const getTasks = () => {
 	console.log('running getTasks function');
@@ -28,6 +60,7 @@ const getTasks = () => {
 };
 
 function renderTasks(list) {
+	$('#taskTable').empty();
 	for (let task of list) {
 		let deadline = new Date(task.deadline).toDateString();
 		let htmlText = $(`<tr></tr>`);
