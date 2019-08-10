0f4e53bbec66de48e0c514ce8debaa592c6c9c19
dmheisel <d.m.heisel@gmail.com>
2019-08-10 11:44:37 -0500

    updated page to add input fields and buttons.  todo - post route to server.

--- database.sql
+++ database.sql
@@ -3,15 +3,18 @@ CREATE TABLE "tasks"
   "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(80) NOT NULL,
   "details" VARCHAR(256),
-  "isDone" BOOLEAN DEFAULT FALSE
+  "created" TIMESTAMP DEFAULT DATE_TRUNC('hour', CURRENT_TIMESTAMP),
+  "deadline" TIMESTAMP DEFAULT DATE_TRUNC('hour', CURRENT_TIMESTAMP + INTERVAL
+  '7 day'),
+	"isDone" BOOLEAN DEFAULT FALSE
 );
 
-INSERT
+  INSERT
 	INTO "tasks"
-  ("name", "details")
-VALUES
-  ('Garbage', 'Take out the garbage.'),
-  ('Mow', 'Mow the lawn and use the weedwhacker this time.'),
-  ('Homework', 'Finish the weekend assignment for Prime.'),
-  ('Bike Maintenance', 'Wash the bike, clean the chains, tune the shifting, change brakepads.'),
-  ('Oil Change - Jess', 'Change the oil in the car, check with Jess if this was done.');
+    ("name", "details")
+  VALUES
+    ('Garbage', 'Take out the garbage.'),
+    ('Mow', 'Mow the lawn and use the weedwhacker this time.'),
+    ('Homework', 'Finish the weekend assignment for Prime.'),
+    ('Bike Maintenance', 'Wash the bike, clean the chains, tune the shifting, change brakepads.'),
+    ('Oil Change - Jess', 'Change the oil in the car, check with Jess if this was done.');
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,12 @@ console.log('js sourced');
 $(document).ready(readyHandler);
 
 function readyHandler() {
+	$('#addButton').on('click', onAddClick )
 	getTasks();
 }
+
+const 
+
 
 const getTasks = () => {
 	console.log('running getTasks function');
@@ -21,8 +25,22 @@ const getTasks = () => {
 		.catch(err => {
 			console.log('error on GET route from server: ', err);
 		});
-}
+};
 
 function renderTasks(list) {
-	
+	for (let task of list) {
+		let deadline = new Date(task.deadline).toDateString();
+		let htmlText = $(`<tr></tr>`);
+		htmlText
+			.append(`<th scope="row"></th>`)
+			.append(`<td>${task.name}</td`)
+			.append(`<td>${task.details}</td>`)
+			.append(`<td>${deadline}</td>`)
+			.append(
+				`<td><button class="btn btn-primary rounded-pill">Done?</button></td>`
+			);
+		htmlText.data('id', task.id);
+		htmlText.data('task', task);
+		$('#taskTable').append(htmlText);
+	}
 }
